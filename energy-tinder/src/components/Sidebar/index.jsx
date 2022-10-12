226b871d232f6c9f35e3b97915314942c4a615fd
hardlywave <denistimofeenko1234@gmail.com>
2022-10-11 20:18:23 -0400

    add navigate bar

--- energy-tinder/src/components/Sidebar/index.jsx
+++ energy-tinder/src/components/Sidebar/index.jsx
@@ -1,7 +1,60 @@
 import "./index.css";
+import { useState } from "react";
 
 const Sidebar = () => {
-  return <h1>Sidebar</h1>;
+  const [navShowed, setNavShowed] = useState(false);
+
+  const onClickHandler = () => {
+    setNavShowed(!navShowed);
+  };
+
+  const links = [
+    {
+      name: "Profile",
+      path: "/",
+    },
+    {
+      name: "Statistic",
+      path: "/",
+    },
+    {
+      name: "Main",
+      path: "/",
+    },
+  ];
+  return (
+    <>
+      <button className="sidebar-icon" onClick={onClickHandler}>
+        <svg
+          width="35"
+          height="25"
+          viewBox="0 0 20 15"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M20 2V0H0V2H20ZM20 4V6H0V4H20ZM20 10V8H0V10H20ZM20 14V12H0V14H20Z"
+            fill="black"
+          />
+        </svg>
+      </button>
+
+      {navShowed && (
+        <nav className="sidebar">
+          <h1 className="sidebar-header">Sidebar</h1>
+          <ul className="sidebar-list">
+            {links.map((link) => (
+              <li className="list-item">
+                <a href={link.path}>{link.name}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      )}
+    </>
+  );
 };
 
 export default Sidebar;
